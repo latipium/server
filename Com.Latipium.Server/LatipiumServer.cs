3db624ea0d269a1e5a0159a0da92f2aaf13e01ee
Zach Deibert <zachdeibert@gmail.com>
2018-05-25 22:22:00 -0500

    Made it possible to add extra database tables

--- Com.Latipium.Server/LatipiumServer.cs
+++ Com.Latipium.Server/LatipiumServer.cs
@@ -1,8 +1,13 @@
 using System;
 using Com.Latipium.Engine;
+using Com.Latipium.Engine.Data;
 
 namespace Com.Latipium.Server {
     class LatipiumServer : ILatipiumServer {
+        public Context OpenDatabaseContext() {
+            return new Context();
+        }
+
         public static void Main(string[] args) {
             LatipiumEngine.Start(new LatipiumServer());
         }
